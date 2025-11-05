3ea396c045e85a070c2629727602ed7a48215814
Daniele Verducci <daniele.verducci@ichibi.eu>
2025-11-05 07:58:28 +0100

    Bumped version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "it.danieleverducci.lunatracker"
         minSdk = 21
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.7"
+        versionCode = 6
+        versionName = "0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
